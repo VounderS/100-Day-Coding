0631d7e136776b4891bd934cd0b22ba27ee5f0cc
Vounder Side <fadil.newbie@gmail.com>
2023-10-18 05:50:33 -0700

    Bubble Sort

---
+++ Day012.java
@@ -0,0 +1,28 @@
+public class Day012 {
+    public static void main(String[] args) {
+        int array[] = { 2, 4, 3, 7, 1, 4, 1 };
+        System.out.print("Before Sorting : ");
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        bubbleSort(array);
+
+    }
+
+    public static void bubbleSort(int[] array) {
+        int arrayLenght = array.length;
+        for (int i = 0; i < arrayLenght - 1; i--) {
+            for (int j = 0; j < arrayLenght - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.print("\nAfter Sorting : ");
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+    }
+}
