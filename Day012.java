0b4731dd623edb3b706d2117f630acb39bc1a06b
Vounder Side <fadil.newbie@gmail.com>
2023-10-19 05:52:16 -0700

    Nested Loop

--- Day012.java
+++ Day012.java
@@ -11,7 +11,7 @@ public class Day012 {
 
     public static void bubbleSort(int[] array) {
         int arrayLenght = array.length;
-        for (int i = 0; i < arrayLenght - 1; i--) {
+        for (int i = 0; i < arrayLenght - 1; i++) {
             for (int j = 0; j < arrayLenght - 1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
