4164d2a20b196a8f9d966cc1d9083061c48be5b3
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:17:39 -0700

    Short Field

---
+++ Day015.java
@@ -0,0 +1,10 @@
+public class Day015 {
+    public static void main(String[] args) {
+        Short a = 1;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+    }
+}
