ec4c28117122a03ea35ef28385f105dc3c0b302b
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:19:28 -0700

    Double Field

---
+++ Day019.java
@@ -0,0 +1,10 @@
+public class Day019 {
+    public static void main(String[] args) {
+        Double a = 1.0;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+    }
+}
