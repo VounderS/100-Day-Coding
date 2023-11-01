0f6653674f6ce59b160544e56d9989a5de0cffab
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:18:56 -0700

    Float Field

---
+++ Day018.java
@@ -0,0 +1,10 @@
+public class Day018 {
+    public static void main(String[] args) {
+        Float a = 1.0f;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+    }
+}
