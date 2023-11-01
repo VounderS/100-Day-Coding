e116f530f0e3c111311e2f95dff6f81ea9c2b088
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:18:36 -0700

    Long Field

---
+++ Day017.java
@@ -0,0 +1,12 @@
+
+public class Day017 {
+    public static void main(String[] args) {
+        Long a = 1L;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+    }
+
+}
