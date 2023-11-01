29879189f73bc3b8b33e85316b856e4222e847a4
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:18:12 -0700

    Integer Field

---
+++ Day016.java
@@ -0,0 +1,11 @@
+
+public class Day016 {
+    public static void main(String[] args) {
+        Integer a = 1;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+    }
+}
