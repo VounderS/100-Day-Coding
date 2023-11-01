c5a21b19b7612f95e0172409eda2bb7ace9982c8
VounderS <fadil.newbie@gmail.com>
2023-11-01 09:08:44 -0700

    Byte Field

---
+++ Day014.java
@@ -0,0 +1,11 @@
+public class Day014 {
+    public static void main(String[] args) {
+        Byte a = 1;
+        System.out.println(a.MAX_VALUE);
+        System.out.println(a.MIN_VALUE);
+        System.out.println(a.SIZE);
+        System.out.println(a.BYTES);
+        System.out.println(a.TYPE);
+
+    }
+}
