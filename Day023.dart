89641f8df557d6da4e9eb8e45aed0893f2012d0d
VounderS <fadil.newbie@gmail.com>
2023-11-05 06:49:48 -0800

    Data Type

---
+++ Day023.dart
@@ -0,0 +1,8 @@
+void main() {
+  int age = 18;
+  double weight = 79.1;
+  String name = "Joko";
+  bool graduate = false;
+
+  print("Name : $name \nAge : $age \nWeight: $weight \nGraduate : $graduate");
+}
