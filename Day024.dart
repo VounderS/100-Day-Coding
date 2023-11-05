10a7863a5eca832456f64f28247eae836b41635d
VounderS <fadil.newbie@gmail.com>
2023-11-05 06:50:56 -0800

    Constant Keyword

---
+++ Day024.dart
@@ -0,0 +1,10 @@
+void main() {
+  //final can only set by once
+  //this keyword is using for set data value that you never change
+  final String name = "Joko";
+  //name = "Your mom ex"; it will give you an error
+  print(name);
+  const int age = 18;
+  //age = 19; const is same aswell
+  print(age);
+}
